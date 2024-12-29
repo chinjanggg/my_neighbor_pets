d3b65fd1582bfa95341125a2e47977dc528a1fb6
chinjanggg <ice_sripaiboon@hotmail.com>
2024-12-30 01:39:51 +0700

    add card item for display pet data in gallery

---
+++ src/components/PetCardItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-card
+        max-width="300"
+        max-height="300"
+        :title="pet.name"
+        :image="imageUrl" >
+    </v-card>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import Pet from '@/models/pet'
+
+const props = defineProps({
+    pet: Pet
+})
+
+const imagePath = `../assets/${props.pet.defaultImage.filename}`
+const imageUrl = computed( () => new URL(imagePath, import.meta.url).href )
+
+</script>
