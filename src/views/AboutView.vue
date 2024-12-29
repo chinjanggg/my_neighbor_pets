33494cf941bfb7a565053de5a8a43f63efdf7c44
chinjanggg <ice_sripaiboon@hotmail.com>
2024-12-29 14:41:30 +0700

    initialize project
    create vue project

---
+++ src/views/AboutView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about">
+        <h1>About "My Neighbor Pets"</h1>
+        เว็บไซต์นี้มีไว้สำหรับลงทะเบียนสัตว์เลี้ยงของลูกบ้าน รวมไปถึงสัตว์จรต่างๆ<br>
+        เพื่อช่วยในการสอดส่องสัตว์หาย หรือหาตัวการที่แท้จริงว่าเป็นน้องจรหรือเปล่า<br>
+        ที่แอบมาอึไว้หน้าบ้าน หรือย่ำฝากระโปรงรถจนลายพร้อย
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+    <main>
+        <img src="@/assets/animal-shelter.png">
+        <br>
+        <a href="https://www.flaticon.com/free-icons/animal-shelter" title="animal shelter icons">Animal shelter icons created by Freepik - Flaticon</a>
+    </main>
+</template>
+
+<script setup>
+</script>
