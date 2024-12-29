3e073af604b354941015e5b18830edbc195a5ae9
chinjanggg <ice_sripaiboon@hotmail.com>
2024-12-29 18:13:58 +0700

    add app bar

---
+++ src/components/Appbar.vue
@@ -0,0 +1,52 @@
+<template>
+    <v-layout>
+        <v-app-bar>
+            <v-app-bar-title>My Neighbor Pets</v-app-bar-title>
+            <v-btn
+                v-for="button in menuList"
+                v-bind:key="button.label"
+                @click="redirect(button.path)">
+                {{ button.label }}
+            </v-btn>
+        </v-app-bar>
+    </v-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const menuList = ref([
+    {
+        label: 'Home',
+        path: '/'
+    },
+    {
+        label: 'About',
+        path: '/about'
+    },
+    {
+        label: 'Gallery',
+        path: '/gallery'
+    },
+    {
+        label: 'Lost And Found',
+        path: '/lostnfound'
+    },
+    {
+        label: 'Authorize',
+        path: '/authorize'
+    },
+])
+
+function redirect( path ) {
+    router.push( path )
+}
+
+</script>
+
+<style>
+
+</style>
