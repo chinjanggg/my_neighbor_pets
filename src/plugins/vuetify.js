efabb39c2f44144a742f050f0e19c4e96f12e80b
chinjanggg <ice_sripaiboon@hotmail.com>
2024-12-29 16:54:14 +0700

    set vuetify config

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { aliases, md } from 'vuetify/iconsets/md'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/styles'
+
+// Vuetify
+
+export default createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'md',
+    aliases,
+    sets: {
+      md,
+    },
+  },
+})
